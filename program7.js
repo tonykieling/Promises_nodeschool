ac3de5d2eb8aea5eb68b0f93856deef42d7ab465
Tony Kieling <tony.kieling@gmail.com>
2019-05-12 09:46:45 -0700

    6 is okay with other way

---
+++ program7.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// first()
+//   .then(message => {
+//     return second(message)})
+//   .then(console.log)
+//   .catch(error => console.log(error)) // it is not necessary, just to do with error handling
+
+first()
+    .then(second())
+    .then(console.log)
+
+
+//   ## Task
+
+// This task will allow you to demonstrate an understanding how to chain promises together using then.
+
+// Two functions will be provided as global functions that you can use: first and second.
+
+// Call the first function in your program. 
+// first() will return a promise that will be fulfilled with a secret value.
+
+// Call the second with the fulfilled value of first.
+// Return the promise returned by second in your onFulfilled callback.
+
+// Finally, print the fulfilled value of that new promise with console.log.
+
