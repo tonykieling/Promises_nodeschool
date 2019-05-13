e40a4ca792e5a0f08560f773a17638850e3b66ac
Tony Kieling <tony.kieling@gmail.com>
2019-05-13 09:10:38 -0700

    12 is done

--- program12.js
+++ program12.js
@@ -17,7 +17,20 @@ There are several things you will want to know:
   * `q-io`'s `http` module has a `read` method which returns a promise for the content of a successful (status 200) HTTP request.
   * Parse the returned JSON and `console.log` it for much win.
 
-This challenge is a bit tricky but the implementation is relatively straightforward.  If you get stuck, refer to the q-io documentation for clarification:
+This challenge is a bit tricky but the implementation is relatively straightforward.  If you get stuck, refer to the q-io documentation for clarification.
 
-  [https://](https://localhost:1337)
+# PASS
+Your solution to Fetch JSON passed!
+Here's the official solution in case you want to compare notes:
+
+────────────────────────────────────────────────────────────────────────────────
+    var qhttp = require('q-io/http');
+    
+    qhttp.read("http://localhost:1337")
+    .then(function (json) {
+      console.log(JSON.parse(json));
+    })
+    .then(null, console.error)
+    .done()
+    
 */
